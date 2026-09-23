eb5697ffa5b2ccc9ad688b5e63116b08747b9f93
funafshin <it.afshin@gmail.com>
2026-09-24 00:42:16 +0330

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='jsky-roster-v1';const A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
